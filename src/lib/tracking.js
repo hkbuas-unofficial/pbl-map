b0502ed752b30d2ff71c79eeeee7d986c1279b88
RacecarLucas <lucas.ly.chen@gmail.com>
2026-06-06 21:02:19 +0800

    chore: update Worker subdomain to hkbuas

--- src/lib/tracking.js
+++ src/lib/tracking.js
@@ -4,7 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const DEVICE_ID_KEY = '@pbl_device_id';
 
 // CHANGE THIS to your Cloudflare Worker URL after deployment
-const API_BASE = 'https://pbl-map-api.s20201020.workers.dev';
+const API_BASE = 'https://pbl-map-api.hkbuas.workers.dev';
 
 let deviceIdCache = null;
 
